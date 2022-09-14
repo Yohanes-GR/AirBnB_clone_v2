1f058267c7331d6192d17ef2ef92f3205e5e31a6
iamyunusali <aliyunus291@gmail.com>
2022-09-14 19:17:26 +0300

    Edit amenity

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    '''amenity class'''
+    '''Amenity class'''
     __tablename__ = 'amenities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
